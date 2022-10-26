e7cec85b127cb26108ebf80ba0ce529755b3043f
Jake <jake.ord345@gmail.com>
2022-10-27 00:20:30 +0100

    fixed base url??

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,17 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../server/init";
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined")
+    // browser should use relative path
+    return "";
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+};
+
 export const client = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: "http://localhost:3000/api" })]
+  links: [httpBatchLink({ url: `${getBaseUrl()}/api` })]
 });
