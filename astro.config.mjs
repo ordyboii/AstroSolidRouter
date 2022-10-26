06ffaf1a408365ee40e04b4309ba3421a69855bb
Jake <jake.ord345@gmail.com>
2022-10-27 00:16:52 +0100

    changed to edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
